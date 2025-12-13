46717aa69be4fb937075e56c39c04c2ef0e6fbb2
Abhishek raj <abhisraj099@gmail.com>
2025-12-14 01:14:46 +0530

    chore(backend): improve Mongo connection logging

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -10,6 +10,12 @@ if (!MONGODB_URI) {
 }
 
 export const connectDB = async () => {
-  await mongoose.connect(MONGODB_URI);
-  console.log("Connected to MongoDB");
+  try {
+    console.log("MONGODB_URI prefix:", MONGODB_URI.slice(0, 40));
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Mongo connection error", err);
+    throw err;
+  }
 };
